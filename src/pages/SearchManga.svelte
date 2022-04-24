f1bac96e87b2d2e1238d9851bab05a9cdec42c40
Tikka710 <yuta.arai.cg@gmail.com>
2022-04-24 09:36:33 +0900

    add: 本検索機能

--- src/pages/SearchManga.svelte
+++ src/pages/SearchManga.svelte
@@ -1,11 +1,22 @@
-<div>
-    <form>
-      <SearchBar bind:value={q} />
-    </form>
-    <div class="text-center mt-4">
-  { q }
-    </div>
-  </div>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <SearchBar bind:value={q} />
+</form>
+
+    {#if empty}
+      <div>検索結果が見つかりませんでした。</div>
+    {:else}
+      {#each mangas as manga (manga.id)}
+        <div>{manga.volumeInfo.title}</div>
+      {/each}
+    {/if}
+    {#await promise}
+    <div>loading...</div>
+    {:catch e}
+      <span class="text-red-600 text-sm">
+        {e.message}
+      </span>
+  {/await}
 
   
 
@@ -13,7 +24,27 @@
 
 <script lang="ts">
 import SearchBar from '../components/SearchBar.svelte'
+import type { Result, MangaItem } from '../repositories/manga'
+import RepositoryFactory, { MANGA } from '../repositories/RepositoryFactory'
 
-let q = 'TypeScript'
+const MangaRepository = RepositoryFactory[MANGA]
+
+let q = ''
+let empty = false
+let mangas: MangaItem[] = []
+let promise: Promise<void>
+
+  const handleSubmit = () => {
+    if (!q.trim()) return
+    promise = getMangas()
+  }
+
+  const getMangas = async () => {
+    mangas = []
+    empty = false
+    const result = await MangaRepository.get({ q })
+    empty = result.totalItems === 0
+    mangas = result.items
+  }
 
 </script>
